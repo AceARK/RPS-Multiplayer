d432b9e8f5781af946f350cff3b4d258b04316ef
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-01-26 02:39:25 -0500

    Getting choices on click of choice.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,9 @@ $("#startButton").on("click", function() {
 	// If number of players = 1, take name as player 1 else player 2
 	player_1_Name = $("#userName").val();
 	$("#player_1_Name").html(player_1_Name);
+
+	$(".displayBeforeStart").hide();
+	$(".choices").attr('disabled', false);
 })
 
 function rpsGameValidate(player_1_Choice, player_2_Choice) {
@@ -23,3 +26,13 @@ function rpsGameValidate(player_1_Choice, player_2_Choice) {
 		player_2_Score ++;
 	}
 }
+
+$(".choices").on("click", function() {
+	if($(this).parent().hasClass('leftSidePanel')) {
+		player_1_Choice = $(this).data('choice');
+	}else {
+		player_2_Choice = $(this).data('choice');
+	}
+	console.log("player 1 " + player_1_Choice + " player 2 " + player_2_Choice);
+	// console.log($(this).data('choice'));
+})
