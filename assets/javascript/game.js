01a4734b48dc9b6f80fdd89676a69d7c8ea9c5dd
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-01-26 02:30:17 -0500

    Adding code to capture name from input field and fill into playerName div.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,25 @@
-var player1Score, player2Score, tie = 0;
-var player1Choice, player2Choice = "";
+var player_1_Score, player_2_Score, tie = 0;
+var player_1_Choice, player_2_Choice = "";
+var player_1_Name, player_2_Name = "";
 
-function rpsGameValidate(player1Choice, player2Choice) {
-	if (player1Choice === player2Choice) {
+$("#startButton").on("click", function() {
+	// If number of players = 1, take name as player 1 else player 2
+	player_1_Name = $("#userName").val();
+	$("#player_1_Name").html(player_1_Name);
+})
+
+function rpsGameValidate(player_1_Choice, player_2_Choice) {
+	if (player_1_Choice === player_2_Choice) {
 
 		tie ++;
 
-	}else if (((player1Choice === 'rock') && (player2Choice === 'scissors')) ||
-			   ((player1Choice === 'paper') && (player2Choice === 'rock')) ||
-			   ((player1Choice === 'scissors') && (player2Choice === 'paper'))) {
+	}else if (((player_1_Choice === 'rock') && (player_2_Choice === 'scissors')) ||
+			   ((player_1_Choice === 'paper') && (player_2_Choice === 'rock')) ||
+			   ((player_1_Choice === 'scissors') && (player_2_Choice === 'paper'))) {
 
-		player1Score ++;
+		player_1_Score ++;
 
 	}else {
-		player2Score ++;
+		player_2_Score ++;
 	}
 }
