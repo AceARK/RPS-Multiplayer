379c8cf064aba71c65519ec359b97599a2b1846c
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-01-28 21:43:21 -0500

    Code to display player names, and message to Guest user(3rd or higher users) about game in progress.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var playerCount = 0;
-var thisWindowPlayer = "";
+var thisWindowPlayer = "Guest";
 var turns = 0;
 // Firebase configuration
 var config = {
@@ -55,7 +55,7 @@ $("#startButton").on("click", function(event) {
 			
 		}
 		// Storing current window player in variable thisWindowPlayer to display customized messages
-		thisWindowPlayer = player_1_Name;
+		thisWindowPlayer = player_2_Name;
 
 		// Storing player 1 db reference in variable
 		player2Ref = database.ref("players/2");
@@ -67,17 +67,54 @@ $("#startButton").on("click", function(event) {
 		turns = 0;
 		database.ref("/turns").set(turns);
 	}
+	// Hide input form
+	$(".displayBeforeStart").hide();
 });
 
 database.ref().on("value", function(snapshot) {
-	// Not displaying player name input and start button if 2 players already exist
-	// P.N. - Player count max val = 2 since this game only handles 2 players
-	// P.N. - Future version will include multiple players being paired to play and handling odd number of players
-	if(playerCount === 2 && thisWindowPlayer == "") {
-		$("#displayBeforeStart").hide();
-		$("#gameMessage").html("Two players are already playing. You may wait until they finish playing or come back later.");
-	}
 	// Getting playerCount from database
 	playerCount = snapshot.child("players").numChildren();
+	// Declaring variables for player 1 and player 2 ref snapshots
+	var player1Snapshot = snapshot.child("players").child("1");
+	var player2Snapshot = snapshot.child("players").child("2");
+	// Getting player names from database
+	player_1_Name = player1Snapshot.child("name").val();
+	player_2_Name = player2Snapshot.child("name").val();
+
+	// Gather player name from database if exists, and display regardless of playerCount or turns
+	// If player1 exists, remove "Waiting for player 1" from both windows
+	if(player_1_Name !== null) {
+		$("#player_1_Name").html(player_1_Name);
+		$("#gameStat1").hide();
+	}
+	if(player_2_Name !== null) {
+		$("#player_2_Name").html(player_2_Name);
+		$("#gameStat2").hide();
+	}
+
+	// Messages to be displayed on both windows are done in onValue() function
+	// Display name of player 1 in leftSidePanel
+	// If thisWindowPlayer is player 1, display playerMessage "Hi name. You are player 1."
+	if(playerCount === 1) {
+		// $("#gameMessage").hide();
+		if(thisWindowPlayer === player_1_Name) {
+			$("#playerMessage").html("Hi " + player_1_Name + ". You are Player 1.");
+		}
+	}else if(playerCount === 2) {
+		// $("#gameMessage").hide();
+		if(thisWindowPlayer === player_2_Name) {
+
+			$("#playerMessage").html("Hi " + player_2_Name + ". You are Player 2.");
+
+		}
+		// For anybody logging in after 2 players exist, disable input form, and display message
+		// P.N. - Player count max val = 2 since this game only handles 2 players
+		// P.N. - Future version will include multiple players being paired to play and handling odd number of players
+		if(thisWindowPlayer === "Guest") {
+			$(".displayBeforeStart").hide();
+			$("#gameMessage").html("Two players are already playing. You may wait until they finish playing or come back later.");
+		}
+
+	}
 
 });
